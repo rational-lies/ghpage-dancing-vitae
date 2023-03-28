45a42c6869a0cae9f91d33bf0aa94a2f64b20560
rational-lies <89226699+rational-lies@users.noreply.github.com>
2023-03-28 12:07:18 -0700

    chore: initializing external repo + configure ghpages

---
+++ next.config.js
@@ -0,0 +1,24 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+const isProd = process.env.NODE_ENV === 'production';
+
+module.exports = withBundleAnalyzer({
+  eslint: {
+    dirs: ['.'],
+  },
+  poweredByHeader: false,
+  trailingSlash: true,
+  basePath: '',
+  // The starter code load resources from `public` folder with `router.basePath` in React components.
+  // So, the source code is "basePath-ready".
+  // You can remove `basePath` if you don't need it.
+  reactStrictMode: true,
+  // Getting NextJS to deploy on GHPages. See here https://github.com/gregrickaby/nextjs-github-pages
+  assetPrefix: isProd ? '/your-github-repo-name/' : '',
+  images: {
+    unoptimized: true,
+  },
+});
